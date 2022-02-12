1f35fb6f88261b4605ee855a331b454e453a4382
IronMan <2012joe@gmail.com>
2022-02-12 17:20:58 +0800

    feat(post): display post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,28 @@
 import fs from 'fs'
 import matter from 'gray-matter'
+import Link from 'next/link'
 import path from 'path'
 import Layout from '../../components/Layout'
+import Post from '../../components/Post'
 
 export default function BlogPage({ posts }) {
   console.log('BlogPage: ', posts)
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <h1 className='text-5xl border-b-4 '>Latest Posts</h1>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, idx) => (
+          <Post post={post} key={idx} />
+        ))}
+      </div>
+
+      <Link href='/blog'>
+        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
+          All Posts
+        </a>
+      </Link>
+    </Layout>
+  )
 }
 
 export async function getStaticProps() {
